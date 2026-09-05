d4d76895f73b57eb707bc892813aae9b91588779
katagir19 <YaroslavN06@gmail.com>
2026-09-05 21:38:55 +0300

    refactor: Переписана полностью архитектура проекта

--- frontend/src/components/EmptyState/EmptyState.jsx
+++ frontend/src/components/EmptyState/EmptyState.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import { Panel, IconWrap, Heading, Description } from './style';
 
 export const EmptyState = ({ icon, title, description }) => {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/tenants')
+      .then((res) => res.json())
+      .then((data) => console.log('Данные из БД:', data));
+  }, []);
+
   return (
     <Panel>
       {icon && <IconWrap>{icon}</IconWrap>}
